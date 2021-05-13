48678c753149573c80766d38600df3631a452154
Danai Xezonaki <danaixezonaki@192.168.1.1>
2021-05-13 13:41:39 +0300

    3 encoders model

--- model/model.py
+++ model/model.py
@@ -3,6 +3,28 @@ import torch.nn.functional as F
 from base import BaseModel
 
 
+class ThreeEncoders(BaseModel):
+    def __init__(self, embedding_size, hidden_size, num_classes, batch_first=False, bidirectional=False):
+        super().__init__()
+        self.batch_first = batch_first
+        self.bidirectional = bidirectional
+        self.encoder1 = nn.LSTM(embedding_size, hidden_size, batch_first=self.batch_first, bidirectional=self.bidirectional)
+        self.encoder2 = nn.LSTM(embedding_size, hidden_size, batch_first=self.batch_first, bidirectional=self.bidirectional)
+        self.context_encoder = nn.LSTM(embedding_size, hidden_size, batch_first=self.batch_first, bidirectional=self.bidirectional)
+        self.linear = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x, turns):
+        for (utt, turn) in zip(x, turns):
+            inputs = torch.cat(self.context, utt)
+            if turn == 0:
+                output = self.encoder1(inputs))
+            else:
+                output = self.encoder2(inputs)
+            self.context, (_, _) = self.context_encoder(output)
+            output = self.linear(output)
+        return output
+
+"""
 class MnistModel(BaseModel):
     def __init__(self, num_classes=10):
         super().__init__()
@@ -20,3 +42,4 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+"""
